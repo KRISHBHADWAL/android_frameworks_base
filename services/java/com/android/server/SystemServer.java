00c3cb93d04ff2995b52e552172d5ba3f9333639
Krish Singh Bhadwal <75715914+KRISHBHADWAL@users.noreply.github.com>
2024-09-20 13:22:19 +0530

    Revert "base: Add mock oplus LinearmotorVibratorService"
    This reverts commit 0f92827f8902f1d6562118b2331570542d98a7b9.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -225,7 +225,6 @@ import com.android.server.twilight.TwilightService;
 import com.android.server.uri.UriGrantsManagerService;
 import com.android.server.usage.UsageStatsService;
 import com.android.server.utils.TimingsTraceAndSlog;
-import com.android.server.vibrator.LinearmotorVibratorService;
 import com.android.server.vibrator.VibratorManagerService;
 import com.android.server.vr.VrManagerService;
 import com.android.server.wearable.WearableSensingManagerService;
@@ -2641,11 +2640,6 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartBackgroundInstallControlService");
             mSystemServiceManager.startService(BackgroundInstallControlService.class);
-            t.traceEnd();
-
-
-            t.traceBegin("StartLinearmotorVibratorService");
-            mSystemServiceManager.startService(LinearmotorVibratorService.class);
             t.traceEnd();
 
             t.traceBegin("StartHealthService");
